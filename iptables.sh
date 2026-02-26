243de309a4221d4addbf8b3e10c9ad9fc71c6c49
Alexander <tigerpard@yandex.ru>
2026-02-26 18:04:13 +0300

    Убран дублирующий ESTABLISHED

--- iptables.sh
+++ iptables.sh
@@ -25,7 +25,6 @@ iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 case "$CURRENT_HOST" in
     "$PROXY")
